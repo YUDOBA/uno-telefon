eefa0c203ac1d200225318088cadfa676d52e9b3
YUDOBA <46269442+YUDOBA@users.noreply.github.com>
2026-09-09 07:19:55 +0300

    V18 cache

--- public/client.js
+++ public/client.js
@@ -1,2 +1,2 @@
-document.write('<script src="/c1.js?v=17"><\/script>');
-document.write('<script src="/c2.js?v=17"><\/script>');
+document.write('<script src="/c1.js?v=18"><\/script>');
+document.write('<script src="/c2.js?v=18"><\/script>');
