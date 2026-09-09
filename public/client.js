ee3d4e7d2d04c0b4031e12991bf246a29eaea1fd
YUDOBA <46269442+YUDOBA@users.noreply.github.com>
2026-09-09 16:50:51 +0300

    V20 index

--- public/client.js
+++ public/client.js
@@ -1,2 +1,2 @@
-document.write('<script src="/c1.js?v=19"><\/script>');
-document.write('<script src="/c2.js?v=19"><\/script>');
+document.write('<script src="/c1.js?v=20"><\/script>');
+document.write('<script src="/c2.js?v=20"><\/script>');
