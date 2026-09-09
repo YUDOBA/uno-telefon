90a255e0a03d8cf135c269aa472961f1e51ea5a8
YUDOBA <46269442+YUDOBA@users.noreply.github.com>
2026-09-09 07:13:11 +0300

    V17 index cache

--- public/client.js
+++ public/client.js
@@ -1,2 +1,2 @@
-document.write('<script src="/c1.js?v=16"><\/script>');
-document.write('<script src="/c2.js?v=16"><\/script>');
+document.write('<script src="/c1.js?v=17"><\/script>');
+document.write('<script src="/c2.js?v=17"><\/script>');
