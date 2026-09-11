6ca5d4b64544f199d2bbbf32d00b9dcf45ae5f40
YUDOBA <46269442+YUDOBA@users.noreply.github.com>
2026-09-12 01:18:34 +0300

    V27 cache

--- public/client.js
+++ public/client.js
@@ -1,2 +1,2 @@
-document.write('<script src="/c1.js?v=26"><\/script>');
-document.write('<script src="/c2.js?v=26"><\/script>');
+document.write('<script src="/c1.js?v=27"><\/script>');
+document.write('<script src="/c2.js?v=27"><\/script>');
