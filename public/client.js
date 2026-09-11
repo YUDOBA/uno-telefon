65f62a1c98afe379b39a4a7566ec308149c1ee93
YUDOBA <46269442+YUDOBA@users.noreply.github.com>
2026-09-12 00:49:37 +0300

    V25 cache bust

--- public/client.js
+++ public/client.js
@@ -1,2 +1,2 @@
-document.write('<script src="/c1.js?v=24"><\/script>');
-document.write('<script src="/c2.js?v=24"><\/script>');
+document.write('<script src="/c1.js?v=25"><\/script>');
+document.write('<script src="/c2.js?v=25"><\/script>');
