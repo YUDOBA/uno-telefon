f84daf2e1a6af9f2d832aad0c94752f09ade88b8
YUDOBA <46269442+YUDOBA@users.noreply.github.com>
2026-09-12 01:25:49 +0300

    V28 cache ve cek yazi

--- public/client.js
+++ public/client.js
@@ -1,2 +1,2 @@
-document.write('<script src="/c1.js?v=27"><\/script>');
-document.write('<script src="/c2.js?v=27"><\/script>');
+document.write('<script src="/c1.js?v=28"><\/script>');
+document.write('<script src="/c2.js?v=28"><\/script>');
