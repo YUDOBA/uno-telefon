760aef68cf2ac9751406267bdf2b3772ccde4a4e
YUDOBA <46269442+YUDOBA@users.noreply.github.com>
2026-09-12 22:22:57 +0300

    V54 gizli kart beyaz

---
+++ public/v54.js
@@ -0,0 +1,6 @@
+function ver(){ return "<p class='sub' style='text-align:center;margin-top:18px'>Uno Telefon V54</p>"; }
+(function(){
+  var s=document.createElement("style");
+  s.textContent=".hand.stowed .ucard,.hand.stowed .ucard.red,.hand.stowed .ucard.blue,.hand.stowed .ucard.green,.hand.stowed .ucard.yellow,.hand.stowed .ucard.black{background:#d8d4c8!important;border-color:#b8b3a6!important;box-shadow:none!important}.hand.stowed .ucard *{visibility:hidden!important}.hand.stowed .fresh-dot{display:none!important}.hand.stowed{opacity:.35}";
+  document.head.appendChild(s);
+})();
