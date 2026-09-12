2912ff10c8e96b5859c5b87cf967fba4bac40095
YUDOBA <46269442+YUDOBA@users.noreply.github.com>
2026-09-12 16:26:16 +0300

    V36 index dogrudan script

--- public/client.js
+++ public/client.js
@@ -1,6 +1,1 @@
-document.write('<script src="/c1.js?v=35"><\/script>');
-document.write('<script src="/c1b.js?v=35"><\/script>');
-document.write('<script src="/c2.js?v=35"><\/script>');
-document.write('<script src="/c3.js?v=35"><\/script>');
-document.write('<script src="/c4.js?v=35"><\/script>');
-document.write('<script src="/vfix.js?v=35"><\/script>');
+/* unused: scripts are in index.html */
