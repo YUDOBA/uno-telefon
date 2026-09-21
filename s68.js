0df8b4889c90efe3ad22c49de18cf8495561b887
YUDOBA <46269442+YUDOBA@users.noreply.github.com>
2026-09-21 13:37:00 +0300

    s68 ses dosyalari placeholder

---
+++ s68.js
@@ -0,0 +1,10 @@
+function sendMp3(res, b64) {
+  res.type("audio/mpeg");
+  res.set("Cache-Control", "public, max-age=86400");
+  res.send(Buffer.from(b64, "base64"));
+}
+try { eval(fs.readFileSync(__dirname + "/s68_draw.js", "utf8")); } catch (e) {}
+try { eval(fs.readFileSync(__dirname + "/s68_play.js", "utf8")); } catch (e) {}
+try { eval(fs.readFileSync(__dirname + "/s68_pass.js", "utf8")); } catch (e) {}
+try { eval(fs.readFileSync(__dirname + "/s68_pen.js", "utf8")); } catch (e) {}
+try { eval(fs.readFileSync(__dirname + "/s68_yd.js", "utf8")); } catch (e) {}
