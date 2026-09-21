bacebe6f378d3d9e6c0eba3519141990d1004017
YUDOBA <46269442+YUDOBA@users.noreply.github.com>
2026-09-21 14:03:02 +0300

    V74: sfx boot duzelt, YUDOBA sesi gercek dosya, client v74

--- s68.js
+++ s68.js
@@ -3,19 +3,8 @@ function sendMp3(res, b64) {
   res.set("Cache-Control", "no-store");
   res.send(Buffer.from(b64, "base64"));
 }
-function sendSfxFile(res, name) {
-  var p = path.join(__dirname, "public", name);
-  if (fs.existsSync(p)) {
-    res.type("audio/mpeg");
-    res.set("Cache-Control", "no-store");
-    return res.sendFile(p);
-  }
-  return false;
-}
-["draw","play","pass","pen","yd"].forEach(function(k){
-  app.get("/sfx-"+k+".mp3", function(req,res){
-    if (sendSfxFile(res, "sfx-"+k+".mp3")) return;
-    try { eval(fs.readFileSync(__dirname + "/s68_" + k + ".js", "utf8")); } catch(e) {}
-    if (!res.headersSent) res.status(404).end();
-  });
-});
+try { eval(fs.readFileSync(__dirname + "/s68_draw.js", "utf8")); } catch (e) {}
+try { eval(fs.readFileSync(__dirname + "/s68_play.js", "utf8")); } catch (e) {}
+try { eval(fs.readFileSync(__dirname + "/s68_pass.js", "utf8")); } catch (e) {}
+try { eval(fs.readFileSync(__dirname + "/s68_pen.js", "utf8")); } catch (e) {}
+try { eval(fs.readFileSync(__dirname + "/s68_yd.js", "utf8")); } catch (e) {}
